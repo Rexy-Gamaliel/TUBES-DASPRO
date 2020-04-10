c26ffb842ee2a43a1e38e9bd43d3ad68ba646e24
rahutami <63393736+rahutami@users.noreply.github.com>
2020-04-10 16:48:55 +0700

    Update cari_pemain.py

--- cari_pemain.py
+++ cari_pemain.py
@@ -1,11 +1,15 @@
 def cari_pemain():
 	username = input("Masukkan username: ")
+	found = False
 	for row in user_file:
 		for field in row:
 			if field == username:
+				found = True
 				print("Nama Pemain: " + row[0])
 				print("Tinggi Pemain: " + row[2] + " cm")
 				print("Tanggal Lahir Pemain: " + row[1])
+	if not found:
+		print("Pemain tidak ditemukan")
         
 ## Program utama
 cari_pemain()
