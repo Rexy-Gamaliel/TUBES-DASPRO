388c04e592d737a8adc823310258e3287acb60e2
rahutami <63393736+rahutami@users.noreply.github.com>
2020-04-18 23:30:28 +0700

    Update refund.py

--- refund.py
+++ refund.py
@@ -1,7 +1,8 @@
-def refund (username):
+def refund ():
+    username=input()
     id_wahana = input("Masukkan ID Wahana: ")
     jumlah = int(input("Jumlah: "))
-
+    tanggal = input("Masukkan tanggal hari ini: ")
     found = False
     i=0 #Untuk tahu row berapa di file_kepemilikan
 
@@ -13,29 +14,29 @@ def refund (username):
                 if temp==0:
                     del file_kepemilikan[i]
                 else:
-                    li=list(row)
-                    li[2]=str(temp)
-                    tup=tuple(li)
-                    file_kepemilikan[i]=tup
-                    print(file_kepemilikan[i])
+                    row[2]=str(temp)
+                    file_kepemilikan[i]=row
                 for row_wahana in file_wahana:
                     if id_wahana == row_wahana[0]:
                         price = int(row_wahana[2])
-                j=0
+                        print(price)
+                j=0 #Untuk tahu row berapa di file_user
                 for row_user in file_user:
                     if username == row_user[3]:
                         temp=int(row_user[6])
                         temp+=price*jumlah
-                        tup=row_user
-                        li=list(tup)
-                        li[6]=str(temp)
-                        tup=tuple(li)
-                        file_user[j]=tup
+                        row_user[6]=str(temp)
+                        file_user[j]=row_user
+                        break
                     j+=1
-                print("Refund berhasil. Saldo anda sekarang " + tup[6])
+                print("Refund berhasil. Saldo anda sekarang " + row_user[6])
             else:
                 print("Anda hanya memiliki " + row[2] + " tiket untuk wahana ini.")
+            break
             i+=1
-            
+
+        li = [username, tanggal, id_wahana, jumlah]
+        file_refund.append(li)
+        
     if not found:
         print("Anda tidak memiliki tiket wahana ini.")
