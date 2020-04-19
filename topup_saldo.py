0025074410502c570c275e3dbba7e15a708e8020
rahutami <63393736+rahutami@users.noreply.github.com>
2020-04-19 09:49:25 +0700

    Update topup_saldo.py

--- topup_saldo.py
+++ topup_saldo.py
@@ -5,7 +5,6 @@ def topup_saldo():
 	for row in user_file:
 		if row[3] == username:
 			found = True
-			temp=int(row[6]) + topup
 			row[6]= int(row[6]) + topup
 			print("Top up berhasil. Saldo " + str(row[0]) + " bertambah menjadi " + str(row[6]))
 	if not found:
