b8e7d9aaf68259211bc800f48f8e07749d413b2f
rahutami <63393736+rahutami@users.noreply.github.com>
2020-04-21 05:58:06 +0700

    Update topup_saldo.py

--- topup_saldo.py
+++ topup_saldo.py
@@ -1,14 +1,16 @@
-def topup_saldo():
-	username=input("Masukkan username: ")
-	topup=int(input("Masukkan saldo yang di-top up: "))
-	found = False
-	for row in user_file:
-		if row[3] == username:
-			found = True
-			row[6]= int(row[6]) + topup
-			print("Top up berhasil. Saldo " + str(row[0]) + " bertambah menjadi " + str(row[6]))
-	if not found:
-		print("Pemain tidak ditemukan")
+def topup():
+    username=input("Masukkan username: ")
+    topup=int(input("Masukkan saldo yang di-top up: "))
+    found = False
+    i=0
+    for row in file_user:
+        if row[3] == username:
+            found = True
+            row[6]= str(int(row[6]) + topup)
+            file_user[i]=row
+            print("Top up berhasil. Saldo " + str(row[0]) + " bertambah menjadi " + str(row[6]))
+        i+=1
+    if not found:
+      print("Pemain tidak ditemukan")
 
-## Program utama
-topup_saldo()
+topup()
