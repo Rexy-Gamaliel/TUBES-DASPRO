bb5cee0f4c51d9e9e83afbd472b713ffa0ca5932
Rexy-Gamaliel <59910292+Rexy-Gamaliel@users.noreply.github.com>
2020-04-23 02:33:58 +0700

    Update load.py

--- load.py
+++ load.py
@@ -1,7 +1,7 @@
 import csv
 
 def toArray (filename):
-    f = open('user.csv', 'r')
+	f = open(filename, 'r')
 	file=csv.reader(f)
 	row=next(file)
 
@@ -19,7 +19,7 @@ def toArray (filename):
 	    kolom+=1
 
 	for row in file:
-	    kolom=0\
+	    kolom=0
 	    for field in row:
 	        arr[baris][kolom]=field
 	        kolom+=1
@@ -29,22 +29,22 @@ def toArray (filename):
 	return(arr)
 
 def load_file():
-		user = input('Masukkan nama File User: ')
-		file_user = toArray(user)
-		wahana = input('Masukkan nama File Daftar Wahana: ')
-		file_wahana = toArray(wahana)
-		beli_tiket = input('Masukkan nama File Pembelian Tiket: ')
-		file_beli = toArray(beli_tiket)
-		guna_tiket = input('Masukkan nama File Penggunaan Tiket: ')
-		file_pakai = toArray(guna_tiket)
-		milik_tiket = input('Masukkan nama File Kepemilikan Tiket: ')
-		file_kepemilikan = toArray(milik_tiket)
-		refund = input('Masukkan nama File Refund Tiket: ')
-		file_refund = toArray(refund)
-		kritiksaran = input('Masukkan nama File Kritik dan Saran: ')
-		file_ks = toArray(kritiksaran)
-		print("File perusahaan Willy Wangky's Chocolate Factory telah di-load.")
-		return(file_user, file_wahana, file_beli, file_pakai, file_kepemilikan, file_refund, file_ks)
+	user = input('Masukkan nama File User: ')
+	file_user = toArray(user)
+	wahana = input('Masukkan nama File Daftar Wahana: ')
+	file_wahana = toArray(wahana)
+	beli_tiket = input('Masukkan nama File Pembelian Tiket: ')
+	file_beli = toArray(beli_tiket)
+	guna_tiket = input('Masukkan nama File Penggunaan Tiket: ')
+	file_pakai = toArray(guna_tiket)
+	milik_tiket = input('Masukkan nama File Kepemilikan Tiket: ')
+	file_kepemilikan = toArray(milik_tiket)
+	refund = input('Masukkan nama File Refund Tiket: ')
+	file_refund = toArray(refund)
+	kritiksaran = input('Masukkan nama File Kritik dan Saran: ')
+	file_ks = toArray(kritiksaran)
+	print("File perusahaan Willy Wangky's Chocolate Factory telah di-load.")
+	return(file_user, file_wahana, file_beli, file_pakai, file_kepemilikan, file_refund, file_ks)
 
 # Ini nanti di fungsi utama ada ini
 # (file_user, file_wahana, file_beli, file_pakai, file_kepemilikan, file_refund, file_ks) = load_file()
