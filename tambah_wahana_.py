27e547e1fecb56ca339aefef4223bf9d28658eff
auliaadila <63394328+auliaadila@users.noreply.github.com>
2020-04-24 07:11:52 +0700

    Add files via upload

---
+++ tambah_wahana_.py
@@ -0,0 +1,29 @@
+#import csv
+import load
+import save
+
+def tambah_wahana ():
+    file_wahana = toArray("wahana.csv")
+    print("Masukkan informasi wahana yang ditambahkan:")
+
+    idwahana = input("Masukkan ID Wahana: ")
+    nama = input("Masukkan Nama Wahana: ")
+    harga = input("Masukkan Harga Tiket: ")
+    umur = input("Batasan umur: ")
+    tinggi = input("Batasan tinggi badan: ")
+    print("\n")
+    print("Info wahana telah ditambahkan!")
+
+    j=0
+    for row_wahana in file_wahana :
+        if(row_wahana[0]=="."):
+            file_wahana[j][0]=idwahana
+            file_wahana[j][1]=nama
+            file_wahana[j][2]=harga
+            file_wahana[j][3]=umur
+            file_wahana[j][4]=tinggi
+            break
+        j+=1
+    fromArray(file_wahana,"wahana.csv")
+    
+tambah_wahana()
