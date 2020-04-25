d47ba7bda7690113fb252b467c1065f0980ffefa
rahutami <63393736+rahutami@users.noreply.github.com>
2020-04-25 14:16:38 +0700

    Update cari_pemain.py

--- cari_pemain.py
+++ cari_pemain.py
@@ -1,13 +1,26 @@
-def cari_pemain():
-    username = input()
+def cari_pemain(file_user):
+    # Mencari Pemain
+    # Fungsi hanya dapat dilakukan oleh admin, validasi dilakukan di fungsi Utama
+    # file_user = ['Nama','Tanggal_Lahir','Tinggi_Badan','Username','Password','Role','Saldo']
 
+    # Kamus Lokal:
+    # username: string
+    # found: boolean
+    # i: integer
+
+    # Mengambil input username pemain yang ingin dicari
+    username = input("Masukkan username user: ")
+
+    # Mencari pemain
     found = False
-    for row in file_user:
-        if row[3] == username:
+    i=0
+    while not found:
+        if file_user[i][3] == username:
             found = True
-            print("Nama Pemain: " + row[0])
-            print("Tinggi Pemain: " + row[2] + " cm")
-            print("Tanggal Lahir Pemain: " + row[1])
+            print("Nama Pemain: " + file_user[i][0])
+            print("Tinggi Pemain: " + file_user[i][2] + " cm")
+            print("Tanggal Lahir Pemain: " + file_user[i][1])
+        i+=1
 
     if not found:
         print("Pemain tidak ditemukan")
